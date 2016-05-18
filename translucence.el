a89e51914c88356b8c480a2981106d784115c331
Hierophantos <silvergodofgold@gmail.com>
2016-05-18 09:16:55 -0700

    fixes arg out of range warnings, adds frame-opacity fns and refactors

--- translucence.el
+++ translucence.el
@@ -1,22 +1,29 @@
 ;; set a few defaults
-;; look at using defcustom to allow customization menu options
-(setq transparency-step              2)
+;; TODO: look at using defcustom to allow customization menu options
+(setq transparency-step              1)
 (setq default-active-transparency   90)
 (setq default-inactive-transparency 80)
-(setq custom-diff                    5)  ;; diff between active/inactive frame
+(setq transparency-diff              5)  ;; diff between active/inactive frame
 (setq min-opacity                   20)  ;; emacs' lower limit
 (setq max-opacity                  100)  ;; emacs' upper limit
 
+(defun frame-opacity (&optional frame)
+  (if frame (frame-parameter frame 'alpha)
+    (frame-parameter (selected-frame) 'alpha)))
+
+(defun active-frame-opacity ()
+  (car (frame-opacity (selected-frame))))
+
+(defun inactive-frame-opacity ()
+  (cadr (frame-opacity (selected-frame))))
+
+(defun opacity-all-frames ()
+  (mapcar 'frame-opacity (frame-list)))
+
 (defun set-frame-opacity (active inactive &optional all-frames-p)
-  (if (equal all-frames-p t)
-      (eval `(modify-all-frames-parameters '((alpha . (,active ,inactive))))))
-  (eval `(modify-frame-parameters nil '((alpha . (,active ,inactive))))))
-
-(defun get-frame-opacity (&optional frame)
-  (frame-parameter frame 'alpha))
-
-(defun get-opacity-all-frames ()
-  (mapcar 'frame-opacity (frames-on-display-list)))
+  (if all-frames-p
+      (eval `(modify-all-frames-parameters '((alpha . (,active ,inactive)))))
+    (eval `(modify-frame-parameters nil '((alpha . (,active ,inactive)))))))
 
 (defun transparency (active inactive &optional all-frames-p)
   "Custom set frame transparency"
@@ -24,44 +31,47 @@
    "nTransparency on active frame:\s
     nTransparency on inactive frame:\s")
   (if (y-or-n-p "Apply to all frames? ")
-      (set-frame-opacity active inactive t)
+      (set-frame-opacity active inactive 't)
     (set-frame-opacity active inactive)))
 
 (defun frame-transparent-p ()
-  (let ((current-frame-opacity (car (frame-parameter (selected-frame) 'alpha))))
-    (or (/= 100 current-frame-opacity)
-        (>  100 current-frame-opacity))))
+  (< (car (frame-opacity)) 100))
 
 (defun toggle-transparency (&optional all-frames-p)
   "Toggles the transparency of the frame(s)"
   (interactive)
   (if (frame-transparent-p)
-      (set-frame-opacity max-opacity (- max-opacity custom-diff) all-frames-p)
+      (set-frame-opacity max-opacity (- max-opacity transparency-diff) all-frames-p)
     (set-frame-opacity
      default-active-transparency default-inactive-transparency all-frames-p)))
 
 (defun increase-opacity (&optional all-frames-p)
   "Decreases the transparency of the current frame"
   (interactive)
-  (let ((active        (setq active   (car  (frame-parameter nil 'alpha))))
-        (inactive      (setq inactive (cadr (frame-parameter nil 'alpha))))
-        (max-opacity-p (> active max-opacity))
-        (differential  (setq differential (- active custom-diff))))
-
-    (progn (setq active (unless max-opacity-p
-                         (+ active transparency-step)))
-           (if (< active differential)
-               (setq inactive (+ inactive transparency-step))
-             (setq inactive differential)))
+  (let ((active        (setq active       (active-frame-opacity)))
+        (inactive      (setq inactive     (inactive-frame-opacity)))
+        (differential  (setq differential (- active transparency-diff)))
+        (max-opacity-p (>= active max-opacity)))
+    (setq active (if max-opacity-p
+                     max-opacity
+                   (+ active transparency-step)))
+    (if (< active differential)
+        (setq inactive (+ inactive transparency-step))
+      (setq inactive differential))
     (set-frame-opacity active inactive all-frames-p)))
 
 (defun decrease-opacity (&optional all-frames-p)
   "Increases the transparency of the current frame"
   (interactive)
-  (let ((active (car (frame-parameter nil 'alpha)))
-        (inactive (cadr (frame-parameter nil 'alpha))))
-    (progn (setq active (- active transparency-step))
-           (setq inactive (- inactive transparency-step)))
+  (let ((active        (setq active   (active-frame-opacity)))
+        (inactive      (setq inactive (inactive-frame-opacity)))
+        (min-opacity-p (<= active min-opacity)))
+    (setq active   (if min-opacity-p
+                       min-opacity
+                       (- active transparency-step)))
+    (setq inactive (if min-opacity-p
+                     min-opacity
+                     (- inactive transparency-step)))
     (set-frame-opacity active inactive all-frames-p)))
 
 ;; ideally, when we increase or decrease opacity on all frames,
@@ -140,23 +150,26 @@
 (global-set-key (kbd "C-M-<double-wheel-down>")   'decrease-opacity)
 (global-set-key (kbd "C-M-<triple-wheel-down>")   'decrease-opacity)
 
-(global-set-key (kbd "C-M-S-<wheel-up>")          (lambda () (interactive) (increase-opacity t)))
-(global-set-key (kbd "C-M-S-<double-wheel-up>")   (lambda () (interactive) (increase-opacity t)))
-(global-set-key (kbd "C-M-S-<triple-wheel-up>")   (lambda () (interactive) (increase-opacity t)))
+(global-set-key (kbd "C-M-S-<wheel-up>")          (lambda () (interactive) (increase-opacity 't)))
+(global-set-key (kbd "C-M-S-<double-wheel-up>")   (lambda () (interactive) (increase-opacity 't)))
+(global-set-key (kbd "C-M-S-<triple-wheel-up>")   (lambda () (interactive) (increase-opacity 't)))
 
-(global-set-key (kbd "C-M-S-<wheel-down>")        (lambda () (interactive) (decrease-opacity t)))
-(global-set-key (kbd "C-M-S-<double-wheel-down>") (lambda () (interactive) (decrease-opacity t)))
-(global-set-key (kbd "C-M-S-<triple-wheel-down>") (lambda () (interactive) (decrease-opacity t)))
+(global-set-key (kbd "C-M-S-<wheel-down>")        (lambda () (interactive) (decrease-opacity 't)))
+(global-set-key (kbd "C-M-S-<double-wheel-down>") (lambda () (interactive) (decrease-opacity 't)))
+(global-set-key (kbd "C-M-S-<triple-wheel-down>") (lambda () (interactive) (decrease-opacity 't)))
 
 (global-set-key (kbd "M-<f2>") 'increase-opacity)
 (global-set-key (kbd "M-<f1>") 'decrease-opacity)
 
-(global-set-key (kbd "H-M-<f2>") (lambda () (interactive) (increase-opacity t))) ;; all-frames
-(global-set-key (kbd "H-M-<f1>") (lambda () (interactive) (decrease-opacity t))) ;; all-frames
+(global-set-key (kbd "H-M-<f2>") (lambda () (interactive) (increase-opacity 't))) ;; all-frames
+(global-set-key (kbd "H-M-<f1>") (lambda () (interactive) (decrease-opacity 't))) ;; all-frames
 
 (global-set-key (kbd "H-0")     'toggle-transparency)
-(global-set-key (kbd "M-H-0")   (lambda () (interactive) (toggle-transparency t))) ;; all-frames
-(global-set-key (kbd "H-[")     (lambda () (interactive) (start-transparency-animation 98 75 0.1 t)))
+(global-set-key (kbd "M-H-0")   (lambda () (interactive) (toggle-transparency 't))) ;; all-frames
+(global-set-key (kbd "H-[")     (lambda () (interactive) (start-transparency-animation 98 75 0.1 't)))
 (global-set-key (kbd "H-]")     'stop-transparency-animation)
+
+(global-set-key (kbd "M-[") 'ns-next-frame)
+(global-set-key (kbd "M-]") 'ns-prev-frame)
 
 (provide 'hieros-transparency)
